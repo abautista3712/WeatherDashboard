7d03c74dd4b0649ecc41a7f19b34721b296c5781
Abelard Bautista <abelardbautista@gmail.com>
2020-02-10 23:55:21 -0800

    Added OpenWeatherApp API and added on-click event to take user input from city name field.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,17 @@ function update() {
 }
 
 setInterval(update(), 3600000);
+
+$("#inputBtnArrow").on("click", function() {
+  var city = $("#inputCity").val();
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=2d2e3d50a761f51d222ae328e374ca3b";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+});
